a536563f83a954481d903e0a455494cfcdeed31f
Miguel Gomes <miguelgomescy@gmail.com>
2024-03-03 18:39:11 -0300

    Primeira Versão

---
+++ assets/JS/script.js
@@ -0,0 +1,12 @@
+function mostrarSenha(elemento, idSenha) {
+    var inputPass = document.getElementById(idSenha);
+    var btnMostrarSenha = elemento;
+
+    if (inputPass.type === 'password') {
+        inputPass.setAttribute('type', 'text');
+        btnMostrarSenha.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
+    } else {
+        inputPass.setAttribute('type', 'password');
+        btnMostrarSenha.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
+    }
+}
